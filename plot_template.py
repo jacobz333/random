68133054cae2d29a7f7b8cde77910817cf7faee5
jzhen24 <jzhen24@illinois.edu>
2025-04-10 23:10:43 -0500

    increase resolution

--- plot_template.py
+++ plot_template.py
@@ -8,8 +8,10 @@ import matplotlib.pyplot as plt # plotting
 mpl.rcParams['axes.grid'] = True            # show the grid
 mpl.rcParams['grid.linestyle'] = '-'
 mpl.rcParams['grid.linewidth'] = 0.75
-mpl.rcParams['grid.alpha'] = 0.5            # grid transparency
+mpl.rcParams['grid.alpha'] = 0.33           # grid transparency
 mpl.rcParams['axes.grid.which'] = 'both'    # show both major and minor axes
 mpl.rcParams['xtick.minor.visible'] = True
 mpl.rcParams['ytick.minor.visible'] = True
-mpl.rcParams['figure.figsize'] = (6.4, 4.8) # default figure size
+mpl.rcParams['figure.figsize'] = (6.4, 4.8) # figure size (default=(6.4,4.8))
+plt.rcParams['figure.dpi'] = 300            # increases resolution (default=100). dpi = dots-per-inch
+plt.rcParams['savefig.dpi'] = 300
